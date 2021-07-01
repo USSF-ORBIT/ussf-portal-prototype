f8539d3d3dc8a935a2413d238b31b2d43d139a58
Suzanne Rozier <suz@truss.works>
2021-07-01 08:50:13 -0500

    Add Node CI Github workflow (#16)
    * Add lint workflow
    
    * Add Jest coverage thresholds, run jest in CI
    
    * This commit has failing tests and eslint errors
    
    * Fix the lint error
    
    * Fix the failing test

--- components/Logo/Logo.test.tsx
+++ components/Logo/Logo.test.tsx
@@ -6,9 +6,18 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Logo from './Logo'
 
-test('Logo is rendered', () => {
-  render(<Logo size={250} />)
+describe('Logo component', () => {
+  it('renders without a size prop', () => {
+    render(<Logo />)
 
-  const image = screen.getByRole('img')
-  expect(image).toHaveAttribute('alt', 'Space Force')
+    const image = screen.getByRole('img')
+    expect(image).toHaveAttribute('alt', 'Space Force')
+  })
+
+  it('renders with a size prop', () => {
+    render(<Logo size={250} />)
+
+    const image = screen.getByRole('img')
+    expect(image).toHaveAttribute('alt', 'Space Force')
+  })
 })
--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,12 @@ module.exports = {
     '\\.(scss|sass|css|png)$': 'identity-obj-proxy',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  coverageThreshold: {
+    global: {
+      statements: 95,
+      branches: 95,
+      functions: 95,
+      lines: 95,
+    },
+  },
 }
