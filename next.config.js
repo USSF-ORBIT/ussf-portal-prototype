0433928e5c32460c3bf87557709917d94e9897d8
Abigail Young <abbyoung@gmail.com>
2021-07-06 11:57:26 -0700

    Remove unnecessary empty next.config.js file

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // sassOptions: {
-  //   includePaths: [
-  //     'node_modules/',
-  //     // what's the best way to handle referencing a library in scss?
-  //     // #TODO: look up this.addDependency and see if relevant
-  //   ],
-  // },
-}
