8a077ea9a193b848440722be78346336523d191a
Abigail Young <abbyoung@gmail.com>
2021-07-06 10:35:38 -0700

    Change version of USWDS to 2.10.3, reorder imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,4 @@
-import '@trussworks/react-uswds/lib/uswds.css'
 import '@trussworks/react-uswds/lib/index.css'
-import 'styles/uswds-theme.scss'
 import 'styles/globals.scss'
 import type { AppProps } from 'next/app'
 
